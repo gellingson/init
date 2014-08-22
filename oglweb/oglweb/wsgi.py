5183d9527f989bb4f638c6614860df8b0cc93751
Glenn Ellingson <geewiz@gmail.com>
2014-08-22 15:13:47 -0700

    monkeypatch of pymysql installed "as MySQLdb" to fool django

--- oglweb/oglweb/wsgi.py
+++ oglweb/oglweb/wsgi.py
@@ -7,6 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
+# django wants to access the db through mysqldb, so we have to "monkey patch"
+# in pymysql as if it were mysqldb.
+# GEE question: I don't recall why I needed this under apache wsgi but not
+# under the django test web server?
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oglweb.settings")
 
