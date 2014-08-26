7ad3ba87224acb9f24233f226b760f1e04ed5c3e
Glenn Ellingson <geewiz@gmail.com>
2014-08-26 12:13:56 -0700

    initial models and session handling through sqlalchemy package

---
+++ orm/models.py
@@ -0,0 +1,211 @@
+# coding: utf-8
+import re
+
+from sqlalchemy import Column, DateTime, ForeignKey, Index, Integer, Numeric, SmallInteger, String, Table, text
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declared_attr
+
+# id column is used on most but not all tables, so make a mixin
+class IDMixIn(object):
+    id =  Column(Integer, primary_key=True)
+
+# extend the base to automagically get non-camelcase tablename
+class Base(object):
+    
+    @declared_attr
+    def __tablename__(cls):
+        s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', cls.__name__)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+    
+    def convert_datetime(value):
+        return value.strftime("%Y-%m-%d %H:%M:%S")
+
+    def todict(self):
+        d = {}
+        for c in self.__table__.columns:
+            if isinstance(c.type, DateTime):
+                value = getattr(self, c.name).strftime("%Y-%m-%d %H:%M:%S")
+            else:    
+                value = getattr(self, c.name)
+            d[c.name] = value
+        return d
+
+    def get_iter(self):
+        for c in self.__table__.columns:
+            if isinstance(c.type, DateTime):
+                value = convert_datetime(getattr(self, c.name))
+            else:
+                value = getattr(self, c.name)
+
+            yield(c.name, value)
+
+    def iterfunc(self):
+        """Returns an iterable that supports .next()
+        so we can do dict(sa_instance)
+        """
+        return self.get_iter()
+
+    def fromdict(self, values):
+        """Merge in items in the values dict into our object if it's one of our columns
+        """
+        for c in self.__table__.columns:
+            if c.name in values:
+                setattr(self, c.name, values[c.name])
+
+    def __str__(self):
+        return str(dict(self))
+
+Base = declarative_base(cls=Base)
+metadata = Base.metadata
+Base.__iter__ = Base.iterfunc
+
+
+class Classified(IDMixIn, Base):
+
+    status = Column(String(1), nullable=False)
+    markers = Column(String(24))
+    primary_classified_id = Column(Integer)
+    textid = Column(String(32), nullable=False)
+    full_name = Column(String(1024), nullable=False)
+    base_url = Column(String(1024))
+    custom_pull_func = Column(String(1024))
+    extract_car_list_func = Column(String(1024))
+    listing_from_list_item_func = Column(String(1024))
+    parse_listing_func = Column(String(1024))
+    inventory_url = Column(String(1024))
+    owner_account_id = Column(Integer)
+
+#        "id: {}, textid: {}, full_name: {}, status: {}, markers: {}\n base_url: {}, inventory_url: {}, custom_pull_func: {}, extract_car_list_func: {}, listing_from_list_item_func: {}, parse_listing_func: {}, owner_account_id: {}, primary_classified_id: {}",
+#              id, textid, full_name, status, markers,
+#              base_url, inventory_url,
+#              custom_pull_func, extract_car_list_func, listing_from_list_item_func, parse_listing_func,
+#              owner_account_id, primary_classified_id)
+
+class Dealership(IDMixIn, Base):
+
+    status = Column(String(1), nullable=False)
+    markers = Column(String(24))
+    primary_dealership_id = Column(Integer)
+    textid = Column(String(32), nullable=False)
+    full_name = Column(String(1024), nullable=False)
+    base_url = Column(String(1024))
+    extract_car_list_func = Column(String(1024))
+    listing_from_list_item_func = Column(String(1024))
+    parse_listing_func = Column(String(1024))
+    inventory_url = Column(String(1024))
+    address_line1 = Column(String(255))
+    address_line2 = Column(String(255))
+    city = Column(String(255))
+    state = Column(String(255))
+    zip = Column(String(255))
+    phone = Column(String(30))
+    owner_info = Column(String(255))
+    license_info = Column(String(255))
+    owner_account_id = Column(Integer)
+    lat = Column(Numeric(10, 7))
+    lon = Column(Numeric(10, 7))
+
+
+class DealershipActivityLog(IDMixIn, Base):
+
+    dealership_id = Column(Integer)
+    activity_timpestamp = Column(DateTime)
+    action_code = Column(String(32))
+    message = Column(String(1024))
+
+
+class InventoryImportLog(IDMixIn, Base):
+
+    source_type = Column(String(1))
+    source_id = Column(Integer)
+    import_timestamp = Column(DateTime)
+    message = Column(String(1024))
+
+
+class Listing(IDMixIn, Base):
+
+    markers = Column(String(24))
+    status = Column(String(1), nullable=False)
+    model_year = Column(String(4))
+    make = Column(String(255))
+    model = Column(String(255))
+    price = Column(Integer)
+    listing_text = Column(String(2048))
+    pic_href = Column(String(2048))
+    listing_href = Column(String(2048))
+    source_type = Column(String(1))
+    source_id = Column(Integer)
+    source_textid = Column(String(255))
+    local_id = Column(String(255))
+    stock_no = Column(String(255))
+    listing_date = Column(DateTime)
+    removal_date = Column(DateTime)
+    last_update = Column(DateTime)
+    lat = Column(Numeric(10, 7))
+    lon = Column(Numeric(10, 7))
+
+
+class NonCanonicalMake(IDMixIn, Base):
+
+    non_canonical_name = Column(String(1024), index=True)
+    canonical_name = Column(String(1024))
+    consume_words = Column(String(1024))
+    push_words = Column(String(1024))
+
+
+
+
+class Tbl02Model(Base):
+
+    model_id = Column(Integer, primary_key=True)
+    model_make_id = Column(String(32), nullable=False, index=True)
+    model_name = Column(String(64), nullable=False, index=True)
+    model_trim = Column(String(64), nullable=False, index=True)
+    model_year = Column(Integer, nullable=False, index=True)
+    model_body = Column(String(64), index=True)
+    model_engine_position = Column(String(8), index=True)
+    model_engine_cc = Column(Integer)
+    model_engine_cyl = Column(Integer)
+    model_engine_type = Column(String(32))
+    model_engine_valves_per_cyl = Column(Integer)
+    model_engine_power_ps = Column(Integer)
+    model_engine_power_rpm = Column(Integer)
+    model_engine_torque_nm = Column(Integer)
+    model_engine_torque_rpm = Column(Integer)
+    model_engine_bore_mm = Column(Numeric(6, 1))
+    model_engine_stroke_mm = Column(Numeric(6, 1))
+    model_engine_compression = Column(String(8))
+    model_engine_fuel = Column(String(32), index=True)
+    model_top_speed_kph = Column(Integer)
+    model_0_to_100_kph = Column(Numeric(4, 1))
+    model_drive = Column(String(16), index=True)
+    model_transmission_type = Column(String(32))
+    model_seats = Column(Integer)
+    model_doors = Column(Integer)
+    model_weight_kg = Column(Integer)
+    model_length_mm = Column(Integer)
+    model_width_mm = Column(Integer)
+    model_height_mm = Column(Integer)
+    model_wheelbase_mm = Column(Integer)
+    model_lkm_hwy = Column(Numeric(4, 1))
+    model_lkm_mixed = Column(Numeric(4, 1))
+    model_lkm_city = Column(Numeric(4, 1))
+    model_fuel_cap_l = Column(Integer)
+    model_sold_in_us = Column(Integer, index=True)
+    model_co2 = Column(Integer)
+    model_make_display = Column(String(32))
+
+
+class Zipcode(Base):
+
+    zip = Column(String(5), primary_key=True, server_default=text("''"))
+    city_upper = Column(String(100))
+    city = Column(String(100))
+    state_code = Column(String(2), index=True)
+    state = Column(String(100))
+    country = Column(String(100))
+    county_upper = Column(String(100))
+    county = Column(String(100))
+    lat = Column(Numeric(10, 7))
+    lon = Column(Numeric(10, 7))
---
+++ orm/session.py
@@ -0,0 +1,12 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+sqla_db_string = 'mysql+pymysql://{}:{}@{}/{}'.format(os.environ['OGL_DB_USERACCOUNT'],
+                                                      os.environ['OGL_DB_USERACCOUNT_PASSWORD'],
+                                                      os.environ['OGL_DB_HOST'],
+                                                      os.environ['OGL_DB'])
+engine = create_engine(sqla_db_string)
+Session = sessionmaker(bind=engine)
+session = Session()
