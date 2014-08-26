a22c85a89a5c4101174612ec84bcad6180af77e8
Glenn Ellingson <geewiz@gmail.com>
2014-08-26 16:05:07 -0700

    added homepage view; prettified listings view

--- oglweb/listings/views.py
+++ oglweb/listings/views.py
@@ -3,18 +3,31 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.utils.datastructures import MultiValueDictKeyError
 
+from bunch import Bunch
+from money import Money
+#import babel
 from elasticsearch import Elasticsearch
-
-# Create your views here.
 
 from listings.models import Listing
 
-
 # Create your views here.
 
+def homepage(request):
+    context = {'fubar': 'barfu'}
+    return render(request, 'listings/homepage.html', context)
+    
 def index(request):
+    def prettify_listing(listing):
+        if listing.price == 1:
+            listing.price = 'Contact for price'
+        else:
+            m = Money(listing.price, 'USD')
+            listing.price = m.format('en_US')
+        return listing
+    
     search_string = None
     listings = []
+    search_criteria = 'latest interesting cars'
     try:
         search_string = request.GET['search_string']
     except MultiValueDictKeyError:
@@ -23,9 +36,14 @@ def index(request):
         es = Elasticsearch()
         search_resp = es.search(index='carbyr-index', doc_type='listing-type', size=50, q=search_string)
         for item in search_resp['hits']['hits']:
-            listings.append(item['_source'])
+            es_listing = prettify_listing(Bunch(item['_source']))
+            listings.append(es_listing)
+        search_criteria = search_string
     else:
         listings = Listing.objects.all().order_by('-last_update')
-    context = {'listings': listings}
+        # GEE TODO: these are Listings, whereas other case are es hashes. Reconcile!!
+
+    context = {'listings': listings,
+               'search_criteria': search_criteria}
     return render(request, 'listings/index.html', context)
 
