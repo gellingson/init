9b905e63d0285c99521e8ba4b41287c5f7c7f087
Glenn Ellingson <geewiz@gmail.com>
2014-08-29 09:39:01 -0700

    monkeying around with sqla tests

--- scripts/sqla_test.py
+++ scripts/sqla_test.py
@@ -1,7 +1,22 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 from orm.models import Classified
+from orm.models import Listing
 from orm.session import session
 
 c = session.query(Classified).get(3)
 print(c)
+
+#listing = session.query(Listing).first()
+#print(listing)
+#print('tagset is: {}'.format('/'.join(listing.tagset)))
+#print('adding tags')
+#listing.add_tag('musclecar')
+#listing.add_tag('hotrod')
+#print('removing tag')
+#listing.remove_tag('hotrod')
+#print(listing)
+#print('tagset is: {}'.format('/'.join(listing.tagset)))
+#session.commit()
+result = session.execute("update listing set markers = concat(ifnull(markers,''), 'P') where source_type = :source_type and source_id = :source_id and status = 'F'", {'source_type': 'D', 'source_id': 1})
+session.commit()
