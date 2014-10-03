062c2089ae1e60bf893d358872f94301e259ee57
Glenn Ellingson <geewiz@gmail.com>
2014-10-02 17:00:27 -0700

    added 2 more hardcoded saved queries & fixed glyphicon spacing

--- oglweb/listings/constants.py
+++ oglweb/listings/constants.py
@@ -16,9 +16,19 @@ VALID_FILTERS = {'miatas': { "term": { "model": "miata"}},
 }
 
 SUGGESTED_SEARCH_LIST = {
+    '_sotw_vette': {
+        'id': '_sotw_vette',
+        'desc': 'C2 Corvettes',
+        'query': {'query': {'filtered': {'query': {'query_string': {'default_operator': 'AND', 'query': 'model_year:[1963 TO 1967] make: chevrolet model:corvette'}}}}}
+    },
+    '_sotw_tesla': {
+        'id': '_sotw_tesla',
+        'desc': 'Tesla Roadsters',
+        'query': {'query': {'filtered': {'query': {'query_string': {'default_operator': 'AND', 'query': 'tesla roadster'}}}}}
+    },
     '_sotw_nb': {
         'id': '_sotw_nb',
-        'desc': '99-05 miatas',
+        'desc': '99-05 MX-5 Miatas',
         'query': {'query': {'filtered': {'query': {'query_string': {'query': 'nb', 'default_operator': 'AND'}}}}, 'sort': [{'_geo_distance': {'unit': 'mi', 'order': 'asc', 'location': {'lon': -121.8818207, 'lat': 37.3415451}}}]}
     }
 }
