4bcb9f75bb5b0e2b69cd001895e36cbaab4d7354
Glenn Ellingson <geewiz@gmail.com>
2015-01-22 19:11:22 -0800

    fixed bug that broke returning the last block of any result set through cars_api()

--- oglweb/listings/views.py
+++ oglweb/listings/views.py
@@ -196,7 +196,7 @@ def cars_api(request, query_ref=None, number=50, offset=0):
                     context['new_only'] = True
                 else:
                     context['new_only'] = False
-                return render(request, LISTINGSAPI, context)
+            return render(request, LISTINGSAPI, context)
 
         else: # no query found to execute
             context['listings'] = []
