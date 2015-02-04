fd98cef95fe870301779ac6d5a744994d16f6136
Glenn Ellingson <geewiz@gmail.com>
2015-02-04 10:05:50 -0800

    now two methods: one that should stay in sync with importer.py lookup & a second one for monkeying around

--- scripts/ebay_testing.py
+++ scripts/ebay_testing.py
@@ -16,7 +16,43 @@ cars = None
 def getparms():
     api = mkt(debug=True, appid=None, config_file='../conf/ebay.yaml',warnings=True)
 
+# method to pull using exactly the same api_request/params as importer.py
+def pull_like_importer():
+    api = finding(debug=True, appid=None, config_file='../conf/ebay.yaml',warnings=True)
+    api_request = {
+        'keywords': 'Camaro', # GEE cheat, not in the importer.py api_request
+        'categoryId': 6001,
+        'GLOBAL-ID': 100,
+        'buyerPostalCode': 95112,
+        'itemFilter': [
+        ],
+        'aspectFilter': [],
+        'affiliate': {'trackingId': 1},
+        'sortOrder': 'CountryDescending',
+        'paginationInput': {
+            'entriesPerPage': 100, # max allowed; higher would be ignored
+            'pageNumber': 1},
+        'outputSelector': ['PictureURLLarge', 'PictureURLSuperSize'],
+        }
+    api_request['aspectFilter'].append(
+        {'aspectName': 'Model Year',
+         'aspectValueName': '1968'})
+    response = api.execute('findItemsAdvanced', api_request)
+    cars = response.json()
+    carsj = json.loads(cars)
+    f = open('ebay_output.json', 'w')
+    f.write(json.dumps(carsj, indent=4, sort_keys=True))
+        
+#    r = response.dict()
+#    for car in r['searchResult']['item']:
+#        print('postal code is : {}'.format(car['postalCode']))
+#    print(response.json())
+#    print('{} cars found'.format(cars['_count']))
+    f.close()
+    return response, cars
     
+# a pull that may resemble but is not identical to importer.py;
+# use for expermientation and/or to pull histograms
 def find():
 
     api = finding(debug=True, appid=None, config_file='../conf/ebay.yaml',warnings=True)
@@ -46,7 +82,7 @@ def find():
         # note that some examples/docs have initcap on 1st word but that is WRONG
 #        'outputSelector': ['categoryHistogram', 'aspectHistogram']
         'outputSelector': ['CategoryHistogram', 'AspectHistogram']
-#        'outputSelector': ['PictureURLLarge', 'PictureURLSuperSize', 'ItemSpecifics'],
+        #        'outputSelector': ['PictureURLLarge', 'PictureURLSuperSize', 'ItemSpecifics', 'GalleryInfo'],
         }
     response = api.execute('findItemsAdvanced', api_request)
     cars = response.json()
@@ -60,5 +96,6 @@ def find():
     return response, cars
 
 if __name__ == "__main__":
-    find()
+#    find()
+    pull_like_importer()
     print("done")
